e3186d908d78397323ad1eec8f00dd831f6e3a41
devmario <crevate.jang@gmail.com>
2018-05-02 04:13:49 +0900

    add xcodebuild archive path, remove disused submodule

--- tools/install_debug.sh
+++ tools/install_debug.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 . ready.sh
 
-xcodebuild -workspace 'Gureum.xcworkspace' -scheme 'OSX' -configuration 'Debug' && \
-cd 'build/Debug' && \
+xcodebuild archive -workspace 'Gureum.xcworkspace' -scheme 'OSX' -configuration 'Debug' -archivePath "$ARCHIVE_PATH" && \
+cd "$ARCHIVE_PATH/Products$INSTDIR" && \
 sudo rm -rf "$INSTDIR/$appname" && \
 sudo cp -R "$appname" "$INSTDIR/"
 sudo killall -15 Gureum
--- tools/ready.sh
+++ tools/ready.sh
@@ -3,7 +3,9 @@ cd ..
 
 PROJDIR=`pwd`
 INSTDIR='/Library/Input Methods'
-appname='Gureum.app'
+title='Gureum'
+appname="$title.app"
+ARCHIVE_PATH="build/$title.xcarchive"
 pkgname=`git tag | tail -n 1`
 
 if [ ! $CONFIGURATION ]; then
